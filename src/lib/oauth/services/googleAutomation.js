7ce22276b4a7f0154cf4aee6bdecafddfbc6252a
Muhammad Farras Jibran <farasjibran@gmail.com>
2026-08-01 08:17:04 +0700

    refactor(oauth): remove browser automation + bulk-import services

--- src/lib/oauth/services/googleAutomation.js
+++
@@ -1,6 +0,0 @@
-// Re-export module for Google automation
-// This module provides a clean interface for Google account automation
-// used by Kiro, CodeBuddy, and Qoder bulk import managers
-
-export { runGoogleAccountAutomation } from "./kiroGoogleAutomation.js";
-export default runGoogleAccountAutomation;
